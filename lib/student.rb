4bf6700a21fdc326c88bd316c5ca5a25eb33bf08
Josh Cesana <joshuacesana@gmail.com>
2018-06-20 20:22:18 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,7 +20,7 @@ class Student
     student = self 
     
     attributes_hash.each_pair do |key, value|
-      student.key = value  
+      student.key.to_sym = value  
     end 
     binding.pry 
   end
