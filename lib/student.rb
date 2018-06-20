65f808d674d1bab05f97541424b0ae6fdea88064
Josh Cesana <joshuacesana@gmail.com>
2018-06-20 20:21:05 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,15 +30,15 @@ class Student
   end
 end
 
-test_student = Student.new({:name=>"Alex Patriquin", :location=>"New York, NY"})
+# test_student = Student.new({:name=>"Alex Patriquin", :location=>"New York, NY"})
 
-student_hash = {:twitter=>"someone@twitter.com",
- :linkedin=>"someone@linkedin.com",
- :github=>"someone@github.com",
- :blog=>"someone@blog.com",
- :profile_quote=>"\"Forget safety. Live where you fear to live. Destroy your reputation. Be notorious.\" - Rumi",
- :bio=>
-  "I was in southern California for college (sun and In-n-Out!), rural Oregon for high school (lived in a town with 1500 people and 3000+ cows), and Tokyo for elementary/middle school."}
+# student_hash = {:twitter=>"someone@twitter.com",
+# :linkedin=>"someone@linkedin.com",
+# :github=>"someone@github.com",
+# :blog=>"someone@blog.com",
+# :profile_quote=>"\"Forget safety. Live where you fear to live. Destroy your reputation. Be notorious.\" - Rumi",
+# :bio=>
+#   "I was in southern California for college (sun and In-n-Out!), rural Oregon for high school (lived in a town with 1500 people and 3000+ cows), and Tokyo for elementary/middle school."}
   
-test_student.add_student_attributes(student_hash) 
+# test_student.add_student_attributes(student_hash) 
 
