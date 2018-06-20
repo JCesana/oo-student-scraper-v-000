2ec9f21d7726bb5c68f1c8877f91d7a974f160ea
Josh Cesana <joshuacesana@gmail.com>
2018-06-20 20:16:04 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,5 +30,13 @@ class Student
   end
 end
 
+student_hash = {:twitter=>"someone@twitter.com",
+ :linkedin=>"someone@linkedin.com",
+ :github=>"someone@github.com",
+ :blog=>"someone@blog.com",
+ :profile_quote=>"\"Forget safety. Live where you fear to live. Destroy your reputation. Be notorious.\" - Rumi",
+ :bio=>
+  "I was in southern California for college (sun and In-n-Out!), rural Oregon for high school (lived in a town with 1500 people and 3000+ cows), and Tokyo for elementary/middle school."}
+  
 student.add_student_attributes(student_hash) 
 
